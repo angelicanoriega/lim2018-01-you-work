b069e5bc8f2e8bcf20b231568db017ae21ae07d4
Maria <mariaanguevara@gmail.com>
2018-10-05 01:14:11 -0500

    envia email

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const name = document.getElementById('name');
 const lastname = document.getElementById('lastname');
+const myselect = document.getElementById('mySelect');
 const id = document.getElementById('Id');
 const btn = document.getElementById('btn');
 const data = document.getElementById('usuari-information');
@@ -11,17 +12,41 @@ const information = {
     name: '',
     lastname: '',
     url: '',
+    email: '',
     date: date,
     hour: hour
 }
-// btn.addEventListener('click',()=>{
-//     information.name=name.value;
-//     information.lastname=lastname.value;
-//     firebase.database().ref(`${date}/${name.value}`).set(information);
-// })
+const paintData = (name, lastname, date, hour, url, directionHtml) => {
+    const div = document.createElement('div');
+    const div2 = document.createElement('div');
+    const div3 = document.createElement('div');
+    const div4 = document.createElement('div');
+    const img = document.createElement('img');
+    const p = document.createElement("p");
+    const text = document.createTextNode('Nombre ' + name + ' Apellido ' + lastname + ' Fecha ' + date + ' Hora ' + hour);
+    p.appendChild(text);
+    img.setAttribute('src', url);
+    img.setAttribute('class', 'img-fluid');
+    div.setAttribute('class', "col-md-12");
+    div2.setAttribute('class', "row");
+    div3.setAttribute('class', "col-md-3");
+    div4.setAttribute('class', "col-md-9");
+    div3.appendChild(img);
+    div4.appendChild(p);
+    div2.appendChild(div3);
+    div2.appendChild(div4);
+    div.appendChild(div2);
+    directionHtml.appendChild(div);
+}
+const email = (email) => {
+    console.log(email);
+    Email.send ("maria.noriega@utec.edu.pe",
+    email,
+    "Este es un tema",
+    "este es el cuerpo",
+    {token: "40ecc9a1-9436-4d02-8b23-35d076eacfd9"});
+}
 
-
-// capturar imagen
 window.addEventListener("DOMContentLoaded", function () {
     const video = document.getElementById('video');
     const localStream = null;
@@ -69,42 +94,23 @@ window.addEventListener("DOMContentLoaded", function () {
         information.name = name.value;
         information.lastname = lastname.value;
         information.url = canvas.toDataURL();
+        information.email = myselect.value;
+        email(information.email);
         firebase.database().ref(`usuarios/${name.value}`).set(information);
     })
 }, false);
-const paintData = (name,lastname,date,hour, url, directionHtml) => {
-    const div = document.createElement('div');
-    const div2 = document.createElement('div');
-    const div3 = document.createElement('div');
-    const div4 = document.createElement('div');
-    const img = document.createElement('img');
-    const p = document.createElement("p");
-    const text = document.createTextNode('Nombre '+name+' Apellido '+lastname+' Fecha '+date+' Hora '+hour);
-    p.appendChild(text);
-    img.setAttribute('src', url);
-    img.setAttribute('class', 'img-fluid');
-    div.setAttribute('class', "col-md-12");
-    div2.setAttribute('class', "row");
-    div3.setAttribute('class', "col-md-3");
-    div4.setAttribute('class', "col-md-9");
-    div3.appendChild(img);
-    div4.appendChild(p);
-    div2.appendChild(div3);  
-    div2.appendChild(div4);
-    div.appendChild(div2);
-    directionHtml.appendChild(div);
-}
-visitantes.addEventListener('click',()=>{
+
+visitantes.addEventListener('click', () => {
     document.getElementById('second').removeAttribute('class');
     document.getElementById('first').setAttribute('class', "hidden");
 
 })
-administración.addEventListener('click',()=>{
+administración.addEventListener('click', () => {
     document.getElementById('three').removeAttribute('class');
     document.getElementById('first').setAttribute('class', "hidden");
     document.getElementById('Visitantes')
     const ref = firebase.database().ref("usuarios");
     ref.on("child_added", snap => {
-        paintData(snap.val().name,snap.val().lastname,snap.val().date, snap.val().hour, snap.val().url, data);
+        paintData(snap.val().name, snap.val().lastname, snap.val().date, snap.val().hour, snap.val().url, data);
     })
 })
